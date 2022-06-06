a43f31cfc1c396d0bde8326b84204b3ab5d23d52
Mor Mordechai Ben <mor6686@gmail.com>
2022-06-06 12:06:33 +0300

    useTimerInterval hook - unit tests

--- client/src/hooks/timers/timerInterval.hook.test.tsx
+++ client/src/hooks/timers/timerInterval.hook.test.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useTimerInterval } from './timerInterval.hook';
 
 describe('useTimerInterval', () => {
@@ -12,21 +12,24 @@ describe('useTimerInterval', () => {
 });
 describe('useTimerInterval startTimer', () => {
   it('check startTimer func - at the beginning', async () => {
+    jest.useFakeTimers();
     const { result } = renderHook(() => useTimerInterval(50));
     act(() => {
       result.current.startTimer();
     });
-    await waitFor(
-      () => {
-        expect(result.current.seconds).toBeGreaterThan(1);
-      },
-      { timeout: 2000 }
-    );
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    expect(result.current.seconds).toBeGreaterThan(1);
   });
   it('check startTimer func - after pausing', async () => {
+    jest.useFakeTimers();
     const { result } = renderHook(() => useTimerInterval(50));
     act(() => {
       result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
     });
     act(() => {
       result.current.pauseTimer();
@@ -34,29 +37,168 @@ describe('useTimerInterval startTimer', () => {
     act(() => {
       result.current.startTimer();
     });
-    await waitFor(
-      () => {
-        expect(result.current.seconds).toBeGreaterThan(1);
-      },
-      { timeout: 2000 }
-    );
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    expect(result.current.seconds).toBeGreaterThan(2);
+  });
+  it('check startTimer func - after restarting', async () => {
+    jest.useFakeTimers();
+    const { result } = renderHook(() => useTimerInterval(50));
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    act(() => {
+      result.current.pauseTimer();
+    });
+    act(() => {
+      result.current.restartTimer();
+    });
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    expect(result.current.seconds).toBeGreaterThan(1);
+  });
+});
+describe('useTimerInterval pauseTimer', () => {
+  it('check pauseTimer func - after starting - Smaller than', async () => {
+    jest.useFakeTimers();
+    const { result } = renderHook(() => useTimerInterval(50));
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    act(() => {
+      result.current.pauseTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    expect(result.current.seconds).toBeLessThan(2);
+  });
+  it('check pauseTimer func - after starting', async () => {
+    jest.useFakeTimers();
+    const { result } = renderHook(() => useTimerInterval(50));
+    let seconds = 0;
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    act(() => {
+      seconds = result.current.seconds;
+      result.current.pauseTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1500);
+    });
+    expect(result.current.seconds).toBe(seconds);
+  });
+});
+describe('useTimerInterval restartTimer', () => {
+  it('check restartTimer func - 2 sec after starting', async () => {
+    jest.useFakeTimers();
+    const { result } = renderHook(() => useTimerInterval(50));
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+    act(() => {
+      result.current.restartTimer();
+    });
+    expect(result.current.seconds).toBe(0);
+  });
+  it('check restartTimer func - 2 sec after starting pause start again and restart', async () => {
+    jest.useFakeTimers();
+    const { result } = renderHook(() => useTimerInterval(50));
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+    act(() => {
+      result.current.pauseTimer();
+    });
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+    act(() => {
+      result.current.restartTimer();
+    });
+    expect(result.current.seconds).toBe(0);
+  });
+  it('check restartTimer func - 1 sec after starting restart pause start again and after 2 sec restart', async () => {
+    jest.useFakeTimers();
+    const { result } = renderHook(() => useTimerInterval(50));
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+    act(() => {
+      result.current.pauseTimer();
+    });
+    act(() => {
+      result.current.restartTimer();
+    });
+    act(() => {
+      result.current.startTimer();
+    });
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+    act(() => {
+      result.current.restartTimer();
+    });
+    expect(result.current.seconds).toBe(0);
   });
 });
 
-describe('useTimerInterval pauseTimer', () => {
-  it('check pauseTimer func - after starting', async () => {
-    const { result } = renderHook(() => useTimerInterval(50));
-    act(() => {
-      result.current.startTimer();
-    });
-    act(() => {
-      result.current.pauseTimer();
-    });
-    await waitFor(
-      () => {
-        expect(result.current.seconds).toBe(0);
-      },
-      { timeout: 2000 }
-    );
-  });
-});
+// describe('useTimerInterval startTimer', () => {
+//   it('check startTimer func - at the beginning', async () => {
+//     const { result } = renderHook(() => useTimerInterval(50));
+//     act(() => {
+//       result.current.startTimer();
+//     });
+//     await waitFor(
+//       () => {
+//         expect(result.current.seconds).toBeGreaterThan(1);
+//       },
+//       { timeout: 2000 }
+//     );
+//   });
+//   it('check startTimer func - after pausing', async () => {
+//     const { result } = renderHook(() => useTimerInterval(50));
+//     act(() => {
+//       result.current.startTimer();
+//     });
+//     act(() => {
+//       result.current.pauseTimer();
+//     });
+//     act(() => {
+//       result.current.startTimer();
+//     });
+//     await waitFor(
+//       () => {
+//         expect(result.current.seconds).toBeGreaterThan(1);
+//       },
+//       { timeout: 2000 }
+//     );
+//   });
+// });
